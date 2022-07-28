b41762024e47d8745109d8a159aaf5210a23e4ea
wh <123@qq.com>
2022-07-28 09:50:26 +0800

    hot-fix commit2

--- src/main/java/com/demo/test/GitTest.java
+++ src/main/java/com/demo/test/GitTest.java
@@ -3,7 +3,7 @@ package com.demo.test;
 public class GitTest {
 
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello world git");
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
